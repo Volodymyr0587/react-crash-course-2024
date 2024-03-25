b7ec1fb9b65e52d059b6ffdfd72a326bf63b86df
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-03-25 12:50:36 +0200

    JSX Crash Course

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 const App = () => {
-  return (
-    <div className="text-5xl">App</div>
-  )
-}
+	const name = "John";
+	const names = ["Brad", "John", "Mary", "Sara"];
+	const loggedIn = true;
+  const styles = {
+    color: 'green',
+    fontSize: '55px'
+  }
 
-export default App
+	return (
+		<>
+			<div className="text-5xl">App</div>
+			<p style={{ color: 'red', fontSize: '24px' }}>Hello {name}</p>
+			<p style={styles}>Hello {name}</p>
+
+			<ul>
+				{names.map((name, index) => (
+					<li key={index}>{name}</li>
+				))}
+			</ul>
+      { loggedIn ? <h1>Hello Member</h1> : <h1>Hello Guest</h1> }
+      { loggedIn ? <h1>Hello Member</h1> : '' }
+      { loggedIn && <h1>Hello Member</h1> }
+		</>
+	);
+};
+
+export default App;
